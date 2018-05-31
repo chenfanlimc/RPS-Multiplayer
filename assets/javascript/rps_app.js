c095a14fcf987552c89b4b61fdcd6f317d990c42
cli <cli@mastercontrol.com>
2018-05-30 18:01:13 -0600

    finished draft of base html, finally started making some js changes

---
+++ assets/javascript/rps_app.js
@@ -0,0 +1,44 @@
+
+var userOne = "";
+var userTwo = "";
+
+
+$(document).ready(function () {
+
+    var game = {
+        beginGame: function(){
+            var rock1 = $("<button>").text("Rock");
+            var paper1 = $("<button>").text("Paper");
+            var scissor1 = $("<button>").text("Scissor");
+            var rock2 = $("<button>").text("Rock");
+            var paper2 = $("<button>").text("Paper");
+            var scissor2 = $("<button>").text("Scissor");
+            $(".player-1-choices").append(rock1);
+            $(".player-1-choices").append(paper1);
+            $(".player-1-choices").append(scissor1);
+            $(".player-2-choices").append(rock2);
+            $(".player-2-choices").append(paper2);
+            $(".player-2-choices").append(scissor2);
+        }
+
+    }
+
+
+    //okay, you're gonna...set the players based on the input in text input field
+    $("#add-user").on("click", function (response) {
+        response.preventDefault();
+        if (userOne === "") {
+            userOne = $("#user-name").val();
+            $(".player-1-info").empty();
+            $(".player-1-info").text("Player One: " + userOne);
+        } else {
+            userTwo = $("#user-name").val();
+            $(".player-2-info").empty();
+            $(".player-2-info").text("Player Two: " + userTwo);
+            game.beginGame();
+        }
+    })
+
+
+
+})
